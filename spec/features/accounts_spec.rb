704e36981d85615e08126aee3191ffd402e4807e
Melanie Matthews <melanie.a.matthews@gmail.com>
2014-09-13 12:22:07 +1000

    link account name to transaction list

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -1,14 +1,16 @@
 require 'rails_helper'
 
 feature "Accounts", :type => :feature do
+
   scenario "User views the accounts list for an account" do
 
+    # given I have an account
   	account = FactoryGirl.create(:account, name: 'My New Account')
 
-  	# go to the accounts index page
+  	# when I go to the accounts index page
     visit "/accounts"
 
-    # should see a list of accounts
+    # then I should see a list of accounts
     expect(page).to have_text("Account Summary")
     expect(page).to have_text("My New Account")
     expect(page).to have_text("Current Balance")
@@ -18,53 +20,82 @@ feature "Accounts", :type => :feature do
 
   scenario "User creates a new Account" do
 
-  	# go to the accounts index page
+  	# when I go to the accounts index page
     visit "/accounts"
 
-    # click on the new button
+    # and click on the new button
     within("h1") do
     	find(".btn").click
     end
 
-    # expect to see a new form
+    # then I expect to see a new form
     expect(page).to have_content("New Account")
     expect(page).to have_selector('form')
 
-    # enter data into the form
+    # when I enter data into the form
     fill_in 'Name', with: 'New Account Name'
     fill_in 'Bank', with: 'New Account Bank'
     fill_in 'Starting balance', with: '10.00'
+
+    # and I click 'save'
 		click_on('Save')
 
-		# should see our new pattern on the index page
+		# then I should see our new pattern on the index page
     expect(page).to have_text("Account was successfully created.")
     expect(page).to have_text("New Account Name")
     expect(page).to have_text("New Account Bank")
+
   end
 
   scenario "User edits an account" do
 
+    # given I have an account
   	account = FactoryGirl.create(:account, name: 'My Edit Account')
 
-  	# go to the patterns index page, select your account and click refresh
+  	# when I go to the accounts index page
     visit "/accounts"
 
-    # click on the edit button for the pattern
-    within('tr', text: 'My Edit Account') do
-    	find('a').click
-    end
+    # and I click on the edit button for the account
+    click_link('edit')
+#    within('tr', text: 'My Edit Account') do
+#    	find('a').click
+#    end
 
-    # should see the edit form for this page
+    # then I should see the edit form for this page
     expect(page).to have_text("Edit Account")
     expect(page).to have_selector('form')
 
-    # edit the pattern
+    # when I edit the pattern
     fill_in('Name', with: "New Account Name")
+
+    # and I click 'save'
     click_on('Save')
 
-    # user should see the new text
+    # then I should see the new account details on the account summary page
     expect(page).to have_text("Account Summary")
     expect(page).to have_text("New Account Name")
+
+  end
+
+  scenario "User views transactions for an account" do
+
+    # given I have an account with 2 transactions
+    account = FactoryGirl.create(:account, name: 'Test Account')
+    FactoryGirl.create(:transaction, account: account, notes: "txn1")
+    FactoryGirl.create(:transaction, account: account, notes: "txn2")
+
+    # and I go to the accounts index page
+    visit "/accounts"
+
+    # when I click on the account name
+    click_link("Test Account")
+
+    # then I see the "Test Account" is selected
+    expect(page).to have_select('account_id', selected: 'Test Account')
+
+    # and I see a list of transactions
+    expect(page).to have_content("txn1")
+    expect(page).to have_content("txn2")
 
   end
 end
