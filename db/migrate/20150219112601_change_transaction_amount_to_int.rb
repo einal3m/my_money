bc9dd0b773eb918d62c922c31cc3766bf9281565
Melanie Matthews <melanie.a.matthews@gmail.com>
2015-02-19 22:50:01 +1100

    changed account and transaction money amounts to store value in cents

--- db/migrate/20150219112601_change_transaction_amount_to_int.rb
+++ db/migrate/20150219112601_change_transaction_amount_to_int.rb
@@ -2,7 +2,8 @@ class ChangeTransactionAmountToInt < ActiveRecord::Migration
   def change
   	execute <<-SQL
     	UPDATE transactions
-    	SET amount = amount * 100
+    	SET amount = amount * 100,
+    	    balance = balance * 100
     SQL
 
     change_column :transactions, :amount, :integer
