e9b8bb603bc68c2de2c38a24c563e62025ff034a
Melanie Matthews <melanie.a.matthews@gmail.com>
2015-02-25 22:51:48 +1100

    fix broken account capybara test

--- spec/features/accounts_spec.rb
+++ spec/features/accounts_spec.rb
@@ -1,39 +1,39 @@
 require 'rails_helper'
 
-feature "Accounts", :type => :feature do
-
-  before(:all) {
+feature 'Accounts', :type => :feature do
+  before(:each) {
     # create a few date ranges
-    FactoryGirl.create(:date_range_option, description: "Current Month", klass: "CurrentMonthDateRange", default: true)
-    FactoryGirl.create(:date_range_option, description: "Custom Dates", klass: "CustomDateRange")
+    FactoryGirl.create(:date_range_option, description: 'Current Month', klass: 'CurrentMonthDateRange', default: true)
+    FactoryGirl.create(:date_range_option, description: 'Custom Dates', klass: 'CustomDateRange')
+    FactoryGirl.create(:category_type, name: 'Expense')
+    FactoryGirl.create(:category_type, name: 'Income')
   }
 
-  scenario "User views the accounts list for an account", :js => true do
-
+  scenario 'User views the accounts list for an account', :js => true do
     # given I have an account
-  	account = FactoryGirl.create(:account, name: 'My New Account')
+    FactoryGirl.create(:account, name: 'My New Account', bank: 'My New Bank')
 
-  	# when I go to the accounts index page
-    visit "/my_money"
+    # when I go to the accounts index page
+    visit '/my_money'
 
     # then I should see a list of accounts
-    expect(page).to have_text("my accounts")
-    expect(page).to have_text("Account Summary")
-    expect(page).to have_text("My New Account")
-    expect(page).to have_text("Current Balance")
-    expect(page).to have_text("Net Worth")
+    expect(page).to have_text('my accounts')
+    expect(page).to have_text('Account Summary')
+    expect(page).to have_text('My New Account')
+    expect(page).to have_text('My New Bank')
+    expect(page).to have_text('Current Balance')
+    expect(page).to have_text('Net Worth')
   end
 
-  scenario "User creates a new Account", :js => true do
-
-  	# when I go to the accounts index page
-    visit "/my_money"
+  scenario 'User creates a new Account', :js => true do
+    # when I go to the accounts index page
+    visit '/my_money'
 
     # and click on the new button
     click_on('new')
 
     # then I expect to see a new form
-    expect(page).to have_content("new account")
+    expect(page).to have_content('new account')
 
     # when I enter data into the form
     fill_in 'name', with: 'New Account Name'
@@ -42,52 +42,51 @@ feature "Accounts", :type => :feature do
     fill_in 'starting_date', with: '09-Sep-2000'
 
     # and I click 'save'
-		click_on('save')
+    click_on('save')
 
-		# then I should see our new pattern on the account summary page
-    expect(page).to have_text("account summary")
-    expect(page).to have_text("New Account Name")
-    expect(page).to have_text("New Account Bank")
+    # then I should see our new account on the account summary page
+    expect(page).to have_content('account summary')
+    expect(page).to have_text('New Account Name')
+    expect(page).to have_text('New Account Bank')
+    expect(page).to have_text('$10.00')
+    expect(page).to have_text('9-Sep-2000')
   end
 
-  scenario "User edits an account", :js => true do
-
+  scenario 'User edits an account', :js => true do
     # given I have an account
-  	account = FactoryGirl.create(:account, name: 'My Edit Account')
+    FactoryGirl.create(:account, name: 'My Edit Account')
 
-  	# when I go to the accounts index page
-    visit "/my_money"
+    # when I go to the accounts index page
+    visit '/my_money'
 
     # and I click on the show button for the account
     click_on('show')
 
     # then I should see the show page
-    expect(page).to have_text("account summary")
+    expect(page).to have_text('account summary')
 
     # when I click on the edit button
-    click_on("edit")
+    click_on('edit')
 
     # then I should see the edit form for this page
-    expect(page).to have_text("Edit Account")
+    expect(page).to have_text('Edit Account')
 
-    # when I edit the pattern
-    fill_in('name', with: "New Account Name")
-
+    # when I edit the account
+    fill_in('name', with: 'New Account Name')
     # and I click 'save'
     click_on('save')
 
     # then I should see the new account details on the account summary page
-    expect(page).to have_text("account summary")
-    expect(page).to have_text("New Account Name")
+    expect(page).to have_text('account summary')
+    expect(page).to have_text('New Account Name')
   end
 
-  scenario "User deletes an account", :js => true do
-  
+  scenario 'User deletes an account', :js => true do
     # given I have an account
-    account = FactoryGirl.create(:account, name: 'My Edit Account')
+    FactoryGirl.create(:account, name: 'My Edit Account')
 
     # when I go to the accounts index page
-    visit "/my_money"
+    visit '/my_money'
 
     # and I click on show
     click_on('show')
