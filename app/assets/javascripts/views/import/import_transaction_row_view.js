8470f84881043a992cb8607b0c76d51dd30affb1
Melanie Matthews <melanie.a.matthews@gmail.com>
2015-03-11 19:40:45 +1100

    update subcategory id in model

--- app/assets/javascripts/views/import/import_transaction_row_view.js
+++ app/assets/javascripts/views/import/import_transaction_row_view.js
@@ -4,7 +4,8 @@ MyMoney.Views.ImportTransactionRowView = Backbone.View.extend({
   tagName: "tr",
 
   events: {
-    "change #category_id": "updateSubcategories"
+    "change #category_id": "updateSubcategorySelect",
+    "change #subcategory_id": "updateSubcategory"
   },
 
   initialize: function() {
@@ -22,11 +23,15 @@ MyMoney.Views.ImportTransactionRowView = Backbone.View.extend({
     }
   },
 
-  updateSubcategories: function() {
+  updateSubcategorySelect: function() {
     this.model.set('category_id', parseInt(this.$('#category_id').val()));
     this.set_current_subcategories();
     var html = selectContent(this.current_subcategories, null);
     this.$el.find('#subcategory_id').html(html);
+  },
+
+  updateSubcategory: function() {
+    this.model.set('subcategory_id', parseInt(this.$('#subcategory_id').val()));
   },
 
   render: function(){
