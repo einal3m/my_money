90c7953adba701eb2608ee0840e17825ea7d7f78
Melanie Matthews <melanie.a.matthews@gmail.com>
2015-03-29 21:30:53 +1100

    refactor reconciliations

--- app/controllers/reconciliations_controller.rb
+++ app/controllers/reconciliations_controller.rb
@@ -2,20 +2,16 @@ class ReconciliationsController < ApplicationController
   before_action :set_reconciliation, only: [:show, :update, :transactions, :reconcile]
   before_action :set_account, only: [:index]
 
-  # GET account/:account_id/reconciliations
   def index
     render json: @account.reconciliations.order(statement_date: :desc)
   end
 
-  # GET account/:account_id/reconciliations/1
   def show
     render json: @reconciliation
   end
 
-  # POST account/:account_id/reconciliations
   def create
     @reconciliation = Reconciliation.new(reconciliation_params)
-p reconciliation_params
     if @reconciliation.save
       render json: @reconciliation, status: :created
     else
@@ -23,26 +19,10 @@ p reconciliation_params
     end
   end
 
-  # PATCH/PUT account/:account_id/reconciliations/1
   def update
-    # finished reconciliation?
-    if (params[:reconciled] && params.has_key?(:transactions))
-      # get transaction data from params
+    if reconciliation_finished?
       transaction_array = params[:transactions]
-
-      # extract ids from array which have been selected
-      ids = transaction_array.map {|t| t[:id]}
-
-      # get transactions from database
-      transactions = Transaction.find(ids)
-
-      # update database
-      ActiveRecord::Base.transaction do
-        transactions.each { |t| t.update(reconciliation: @reconciliation) }
-
-        # set reconciliation finished
-        @reconciliation.update(reconciled: true)
-      end
+      reconcile_transactions(transaction_array)
       render json: @reconciliation, status: :ok
     else
       if @reconciliation.update(reconciliation_params)
@@ -51,21 +31,35 @@ p reconciliation_params
         render json: @reconciliation.errors, status: :unprocessable_entity
       end
     end
-
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_reconciliation
-      @reconciliation = Reconciliation.find(params[:id])
-    end
 
-    def set_account
-      @account = Account.find(params[:account_id])
-    end
+  def reconciliation_finished?
+    params[:reconciled] && params.key?(:transactions)
+  end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def reconciliation_params
-      params.require(:reconciliation).permit(:account_id, :statement_date, :statement_balance, :last_reconciled_date, :last_reconciled_balance, :reconciled)
+  def reconcile_transactions(transaction_array)
+    ids = transaction_array.map { |t| t[:id] }
+    transactions = Transaction.find(ids)
+
+    ActiveRecord::Base.transaction do
+      transactions.each { |t| t.update(reconciliation: @reconciliation) }
+      @reconciliation.update(reconciled: true)
     end
+  end
+
+  def set_reconciliation
+    @reconciliation = Reconciliation.find(params[:id])
+  end
+
+  def set_account
+    @account = Account.find(params[:account_id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def reconciliation_params
+    params.require(:reconciliation).permit(:account_id, :statement_date, :statement_balance,
+                                           :last_reconciled_date, :last_reconciled_balance, :reconciled)
+  end
 end
--- spec/controllers/reconciliations_controller_spec.rb
+++ spec/controllers/reconciliations_controller_spec.rb
@@ -1,7 +1,6 @@
 require 'rails_helper'
 
-RSpec.describe ReconciliationsController, :type => :controller do
-
+RSpec.describe ReconciliationsController, type: :controller do
   let(:valid_attributes) {
     FactoryGirl.attributes_for(:reconciliation)
   }
@@ -12,41 +11,41 @@ RSpec.describe ReconciliationsController, :type => :controller do
 
   let(:valid_session) { {} }
 
-  describe "GET index" do
-    it "returns all reconciliations for the account" do
+  describe 'GET index' do
+    it 'returns all reconciliations for the account' do
       reconciliation = FactoryGirl.create(:reconciliation)
-      get :index, {account_id: reconciliation.account_id}, valid_session
+      get :index, { account_id: reconciliation.account_id }, valid_session
 
       expect(response).to be_success
 
       json = JSON.parse(response.body)
       expect(json['reconciliations'].length).to eq(1)
-      expect(json['reconciliations'][0]).to eq(serialize_reconciliation(reconciliation)) 
+      expect(json['reconciliations'][0]).to eq(serialize_reconciliation(reconciliation))
     end
   end
 
-  describe "GET show" do
-    it "returns the specified reconciliation" do
+  describe 'GET show' do
+    it 'returns the specified reconciliation' do
       reconciliation = FactoryGirl.create(:reconciliation)
-      get :show, {account_id: reconciliation.account_id, id: reconciliation.id}, valid_session
+      get :show, { account_id: reconciliation.account_id, id: reconciliation.id }, valid_session
       json = JSON.parse(response.body)
       expect(response).to be_success
       expect(json['reconciliation']).to eq(serialize_reconciliation(reconciliation))
     end
   end
 
-  describe "POST create" do
+  describe 'POST create' do
     let(:account) { FactoryGirl.create(:account) }
 
-    context "with valid params" do
-      it "creates a new Reconciliation" do
+    context 'with valid params' do
+      it 'creates a new Reconciliation' do
         expect {
-          post :create, {account_id: account, reconciliation: build_attributes(:reconciliation)}, valid_session
+          post :create, { account_id: account, reconciliation: build_attributes(:reconciliation) }, valid_session
         }.to change(Reconciliation, :count).by(1)
       end
 
-      it "returns the reconciliation" do
-        post :create, {account_id: account, reconciliation: build_attributes(:reconciliation)}, valid_session
+      it 'returns the reconciliation' do
+        post :create, { account_id: account, reconciliation: build_attributes(:reconciliation) }, valid_session
         expect(response).to be_success
 
         reconciliation = Reconciliation.first
@@ -55,28 +54,28 @@ RSpec.describe ReconciliationsController, :type => :controller do
       end
     end
 
-    context "with invalid params" do
-      it "does not create a new reconciliation" do
+    context 'with invalid params' do
+      it 'does not create a new reconciliation' do
         expect {
-          post :create, {account_id: account, reconciliation: build_attributes(:reconciliation_invalid)}, valid_session
+          post :create, { account_id: account, reconciliation: build_attributes(:reconciliation_invalid) }, valid_session
         }.not_to change(Reconciliation, :count)
         expect(response.status).to be(422)
       end
     end
   end
 
-  describe "PUT update" do
+  describe 'PUT update' do
     let(:reconciliation) { FactoryGirl.create(:reconciliation) }
 
-    context "with valid params" do
-
-      it "updates and returns the requested reconciliation" do
-        put :update, {:account_id => reconciliation.account_id, :id => reconciliation.id, 
-          :reconciliation => build_attributes(:reconciliation,
-              statement_balance: "977",
-              statement_date: "2014-07-02")}, valid_session
+    context 'with valid params' do
+      it 'updates and returns the requested reconciliation' do
+        put :update, {
+          account_id: reconciliation.account_id,
+          id: reconciliation.id,
+          reconciliation: build_attributes(:reconciliation, statement_balance: '977', statement_date: '2014-07-02')
+        }, valid_session
         reconciliation.reload
-        expect(reconciliation.statement_date).to eq(Date.parse("2014-07-02"))
+        expect(reconciliation.statement_date).to eq(Date.parse('2014-07-02'))
         expect(reconciliation.statement_balance).to eq(977)
 
         expect(response).to be_success
@@ -85,29 +84,23 @@ RSpec.describe ReconciliationsController, :type => :controller do
       end
     end
 
-    context "with transactions" do
-
-      it "sets reconciled to true and updates transactions" do
+    context 'with transactions' do
+      it 'sets reconciled to true and updates transactions' do
         r = FactoryGirl.create(:reconciliation, reconciled: false)
         t = FactoryGirl.create(:transaction, account: r.account, reconciliation: nil)
-     
-        put :update, {account_id: r.account_id, 
-                      id: r.id, 
-                      reconciled: true,
-                      :transactions => [{id: t.id}]}
+        put :update, account_id: r.account_id, id: r.id, reconciled: true, transactions: [{ id: t.id }]
 
         r.reload
         t.reload
-
         expect(r.reconciled).to be_truthy
         expect(t.reconciliation).to eq(r)
       end
     end
 
-    context "with invalid params" do
-      it "returns an error code" do
-        put :update, {:account_id => reconciliation.account_id, :id => reconciliation.id, 
-          :reconciliation => build_attributes(:reconciliation_invalid)}, valid_session
+    context 'with invalid params' do
+      it 'returns an error code' do
+        put :update, { account_id: reconciliation.account_id, id: reconciliation.id,
+                       reconciliation: build_attributes(:reconciliation_invalid) }, valid_session
         expect(response.status).to be(422)
       end
     end
