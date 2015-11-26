fb627aeca067698945c628db27af12a1813bfe96
Melanie Matthews <melanie.a.matthews@gmail.com>
2015-11-26 21:07:18 +1100

    Add account filter component

---
+++ web/src/js/components/common/__tests__/account-filter-test.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import shallowRenderer from '../../../util/__tests__/shallow-renderer';
+import AccountFilter from '../account-filter';
+import { Input } from 'react-bootstrap';
+
+describe('AccountFilter', () => {
+  describe('render', () => {
+    it('has a select with account groups', () => {
+      let accountGroups = [
+        { code: 'savings', name: 'Savings', accounts: [{ id: 2, name: 'Account 2' }] },
+        { code: 'other', name: 'Other', accounts: [] },
+        { code: 'share', name: 'Share', accounts: [ { id: 1, name: 'Account 1' }, { id: 3, name: 'Account 3' } ] }
+      ];
+
+      let dropdown = shallowRenderer(<AccountFilter accountGroups={accountGroups}/>);
+
+      expect(dropdown.props.children.type).toEqual(Input);
+      let [accountGroup1, accountGroup2] = dropdown.props.children.props.children;
+
+      expect(accountGroup1.props.label).toEqual('Savings Accounts');
+      expect(accountGroup1.type).toEqual('optgroup');
+
+      let account2 = accountGroup1.props.children[0];
+      expect(account2.type).toEqual('option');
+      expect(account2.props.children).toEqual('Account 2');
+
+      expect(accountGroup2.props.label).toEqual('Share Accounts');
+
+      let account1 = accountGroup2.props.children[0];
+      expect(account1.type).toEqual('option');
+      expect(account1.props.children).toEqual('Account 1');
+
+      let account3 = accountGroup2.props.children[1];
+      expect(account3.type).toEqual('option');
+      expect(account3.props.children).toEqual('Account 3');
+    });
+  });
+});
---
+++ web/src/js/components/common/account-filter.js
@@ -0,0 +1,52 @@
+'use strict';
+
+import React from 'react';
+import { Input } from 'react-bootstrap';
+
+export default class AccountFilter extends React.Component {
+  renderAccountGroups() {
+    let options = [];
+    this.props.accountGroups.forEach((accountGroup) => {
+      if (accountGroup.accounts.length > 0) {
+        options.push(this.renderAccountGroup(accountGroup));
+      }
+    });
+    return options;
+  }
+
+  renderAccountGroup(accountGroup) {
+    return (
+      <optgroup key={accountGroup.code} label={accountGroup.name + ' Accounts'}>
+        {this.renderAccounts(accountGroup.accounts)}
+      </optgroup>
+    );
+  }
+
+  renderAccounts(accounts) {
+    return accounts.map((account) => {
+      return <option key={account.id} value={account.id}>{account.name}</option>;
+    });
+  }
+
+  render() {
+    return (
+      <form className="form-horizontal">
+        <Input type="select" label="Account" labelClassName="col-xs-4" wrapperClassName="col-xs-8">
+          {this.renderAccountGroups()}
+        </Input>
+      </form>
+    );
+  }
+}
+
+AccountFilter.propTypes = {
+  accountGroups: React.PropTypes.arrayOf(React.PropTypes.shape({
+    code: React.PropTypes.string.isRequired,
+    name: React.PropTypes.string.isRequired,
+    accounts: React.PropTypes.arrayOf(React.PropTypes.shape({
+      id: React.PropTypes.number.isRequired,
+      name: React.PropTypes.string.isRequired
+    })).isRequired
+  })).isRequired
+};
+
