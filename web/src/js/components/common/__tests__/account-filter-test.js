a36272202d8aaf9477e8035c2208a472108df974
Melanie Matthews <melanie.a.matthews@gmail.com>
2015-12-06 13:24:04 +1100

    Add onChange test to account filter

--- web/src/js/components/common/__tests__/account-filter-test.js
+++ web/src/js/components/common/__tests__/account-filter-test.js
@@ -1,23 +1,34 @@
 import React from 'react';
 import shallowRenderer from '../../../util/__tests__/shallow-renderer';
 import AccountFilter from '../account-filter';
+import TestUtils from 'react-addons-test-utils';
 import { Input } from 'react-bootstrap';
+import { fromJS } from 'immutable';
 
 describe('AccountFilter', () => {
+  let accountTypes, accountGroup, currentAccount;
+  beforeEach(() => {
+    accountTypes = fromJS([
+      { id: 1, code: 'savings', name: 'Savings' },
+      { id: 3, code: 'other', name: 'Other' },
+      { id: 2, code: 'share', name: 'Share' }
+    ]);
+
+    currentAccount = { id: 1, name: 'Account 1' };
+    accountGroups = fromJS({
+      'savings': [ { id: 2, name: 'Account 2' } ],
+      'share': [ currentAccount, { id: 3, name: 'Account 3' } ]
+    });
+  });
+
   describe('render', () => {
     it('has a select with account groups', () => {
-      let account = { id: 1, name: 'Account 1' };
-      let accountGroups = [
-        { code: 'savings', name: 'Savings', accounts: [{ id: 2, name: 'Account 2' }] },
-        { code: 'other', name: 'Other', accounts: [] },
-        { code: 'share', name: 'Share', accounts: [ account, { id: 3, name: 'Account 3' } ] }
-      ];
-
-      let accountFilter = shallowRenderer(<AccountFilter accountGroups={accountGroups} currentAccount={account}/>);
+      let accountFilter = shallowRenderer(<AccountFilter accountTypes={accountTypes} accountGroups={accountGroups} 
+          currentAccount={fromJS(currentAccount)}/>);
       let dropdown = accountFilter.props.children.props.children;
 
       expect(dropdown.type).toEqual(Input);
-      expect(dropdown.props.defaultValue).toEqual(account.id);
+      expect(dropdown.props.defaultValue).toEqual(currentAccount.id);
       let [accountGroup1, accountGroup2] = dropdown.props.children;
 
       expect(accountGroup1.props.label).toEqual('Savings Accounts');
@@ -36,6 +47,18 @@ describe('AccountFilter', () => {
       let account3 = accountGroup2.props.children[1];
       expect(account3.type).toEqual('option');
       expect(account3.props.children).toEqual('Account 3');
+    });
+  });
+
+  describe('onChange', () => {
+    it('calls the onChange props with the id of the account', () => {
+      let onChangeSpy = jasmine.createSpy('onChange');
+      let accountFilter = TestUtils.renderIntoDocument(<AccountFilter accountTypes={accountTypes} accountGroups={accountGroups} 
+          currentAccount={fromJS(currentAccount)} onChange={onChangeSpy}/>);
+
+      accountFilter.refs.accountSelect.props.onChange({target: {value: '6'}});
+
+      expect(onChangeSpy).toHaveBeenCalledWith(6);
     });
   });
 });
--- web/src/js/components/common/account-filter.js
+++ web/src/js/components/common/account-filter.js
@@ -2,6 +2,7 @@
 
 import React from 'react';
 import { Input } from 'react-bootstrap';
+import { toJS } from 'immutable';
 
 export default class AccountFilter extends React.Component {
   onChange(event) {
@@ -10,34 +11,35 @@ export default class AccountFilter extends React.Component {
 
   renderAccountGroups() {
     let options = [];
-    this.props.accountGroups.forEach((accountGroup) => {
-      if (accountGroup.accounts.length > 0) {
-        options.push(this.renderAccountGroup(accountGroup));
+    this.props.accountTypes.forEach((accountType) => {
+      let accounts = this.props.accountGroups.get(accountType.get('code'));
+      if (accounts) {
+        options.push(this.renderAccountGroup(accountType, accounts));
       }
     });
     return options;
   }
 
-  renderAccountGroup(accountGroup) {
+  renderAccountGroup(accountType, accounts) {
     return (
-      <optgroup key={accountGroup.code} label={accountGroup.name + ' Accounts'}>
-        {this.renderAccounts(accountGroup.accounts)}
+      <optgroup key={accountType.get('code')} label={accountType.get('name') + ' Accounts'}>
+        {this.renderAccounts(accounts)}
       </optgroup>
     );
   }
 
   renderAccounts(accounts) {
     return accounts.map((account) => {
-      return <option key={account.id} value={account.id}>{account.name}</option>;
-    });
+      return <option key={account.get('id')} value={account.get('id')}>{account.get('name')}</option>;
+    }).toJS();
   }
 
   render() {
     return (
       <div className="row">
         <div className="form-horizontal col-xs-4">
-          <Input type="select" label="Account" defaultValue={this.props.currentAccount.id} labelClassName="col-xs-4" 
-                 wrapperClassName="col-xs-8" onChange={this.onChange.bind(this)}>
+          <Input type="select" label="Account" defaultValue={this.props.currentAccount.get('id')} labelClassName="col-xs-4" 
+                 wrapperClassName="col-xs-8" onChange={this.onChange.bind(this)} ref='accountSelect'>
             {this.renderAccountGroups()}
           </Input>
         </div>
@@ -46,17 +48,17 @@ export default class AccountFilter extends React.Component {
   }
 }
 
-let accountProps = React.PropTypes.shape({
-  id: React.PropTypes.number.isRequired,
-  name: React.PropTypes.string.isRequired
-});
+// let accountProps = React.PropTypes.shape({
+//   id: React.PropTypes.number.isRequired,
+//   name: React.PropTypes.string.isRequired
+// });
 
-AccountFilter.propTypes = {
-  currentAccount: accountProps.isRequired,
-  accountGroups: React.PropTypes.arrayOf(React.PropTypes.shape({
-    code: React.PropTypes.string.isRequired,
-    name: React.PropTypes.string.isRequired,
-    accounts: React.PropTypes.arrayOf(accountProps).isRequired
-  })).isRequired
-};
+// AccountFilter.propTypes = {
+//   currentAccount: accountProps.isRequired,
+//   accountGroups: React.PropTypes.arrayOf(React.PropTypes.shape({
+//     code: React.PropTypes.string.isRequired,
+//     name: React.PropTypes.string.isRequired,
+//     accounts: React.PropTypes.arrayOf(accountProps).isRequired
+//   })).isRequired
+// };
 
