5b6b260c4776c95c084bc8bef551249c3827c612
Melanie Matthews <melanie.a.matthews@gmail.com>
2016-03-09 16:44:04 +1100

    Add some new colors to line chart

--- web/src/js/selectors/account-balance-selector.js
+++ web/src/js/selectors/account-balance-selector.js
@@ -5,7 +5,7 @@ const accountSelector = state => state.accountStore.get('accounts');
 const accountBalancesSelector = state => state.reportStore.get('accountBalances');
 const selectedAccountsSelector = state => state.accountStore.get('selectedAccounts');
 
-let chartColours = ['#61ABDB', '#FDCA3A', '#80D8C4'];
+let chartColours = ['#9467bd', '#bcbd22', '#17becf', '#e377c2', '#ffbb78'];
 
 function lineSeriesData(accounts, accountBalances, selectedAccounts) {
   let seriesData = List([]);
@@ -14,7 +14,7 @@ function lineSeriesData(accounts, accountBalances, selectedAccounts) {
       seriesData = seriesData.push(Map({
         name: accounts.find(account => account.get('id') == accountId).get('name'),
         data: accountBalances.get(accountId).map(point => List([new Date(point.get(0)), point.get(1)/100.0])),
-        backgroundColour: chartColours[index]
+        backgroundColour: chartColours[index % 5]
       }));
     }
   });
