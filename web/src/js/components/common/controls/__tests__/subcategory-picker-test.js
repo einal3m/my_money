9fe8137845b4a5c9e5879c4b7b0c8d6493777147
Melanie Matthews <melanie.a.matthews@gmail.com>
2016-11-13 21:34:24 +1100

    Use new drop down component for subcategory picker

--- web/src/js/components/common/controls/__tests__/subcategory-picker-test.js
+++ web/src/js/components/common/controls/__tests__/subcategory-picker-test.js
@@ -1,32 +1,30 @@
 import React from 'react';
 import shallowRenderer from '../../../../util/__tests__/shallow-renderer';
-
 import SubcategoryPicker from '../subcategory-picker';
-import Picker from '../picker';
+import DropDown from '../drop-down';
 
 describe('SubcategoryPicker', () => {
-  let subcategoryPicker,
-    subcategories,
-    onChangeSpy,
-    groupedCategories;
+  const subcategories = [
+    { id: 1, categoryId: 11, name: 'One' },
+    { id: 2, categoryId: 12, name: 'One Again' },
+    { id: 3, categoryId: 11, name: 'Two' },
+    { id: 4, categoryId: 13, name: 'Two Again' },
+    { id: 5, categoryId: 11, name: 'Three' },
+  ];
+
+  const category1 = { id: 11, name: 'Wages', subcategories: [subcategories[0], subcategories[2], subcategories[4]] };
+  const category2 = { id: 12, name: 'Bills', subcategories: [subcategories[1]] };
+  const category3 = { id: 13, name: 'Tax Return', subcategories: [subcategories[3]] };
+
+  const groupedCategories = [
+    { categoryType: { name: 'Income', id: 4 }, categories: [category1, category3] },
+    { categoryType: { name: 'Expense', id: 5 }, categories: [category2] },
+  ];
+
+  let subcategoryPicker;
+  let onChangeSpy;
+
   beforeEach(() => {
-    subcategories = [
-      { id: 1, categoryId: 11, name: 'One' },
-      { id: 2, categoryId: 12, name: 'One Again' },
-      { id: 3, categoryId: 11, name: 'Two' },
-      { id: 4, categoryId: 13, name: 'Two Again' },
-      { id: 5, categoryId: 11, name: 'Three' },
-    ];
-
-    const category1 = { id: 11, name: 'Wages', subcategories: [subcategories[0], subcategories[2], subcategories[4]] };
-    const category2 = { id: 12, name: 'Bills', subcategories: [subcategories[1]] };
-    const category3 = { id: 13, name: 'Tax Return', subcategories: [subcategories[3]] };
-
-    groupedCategories = [
-      { categoryType: { name: 'Income', id: 4 }, categories: [category1, category3] },
-      { categoryType: { name: 'Expense', id: 5 }, categories: [category2] },
-    ];
-
     onChangeSpy = jasmine.createSpy('onChangeSpy');
   });
 
@@ -36,7 +34,7 @@ describe('SubcategoryPicker', () => {
         <SubcategoryPicker groupedCategories={groupedCategories} value={null} onChange={onChangeSpy} categoryId={11} />
       );
 
-      expect(subcategoryPicker.type).toEqual(Picker);
+      expect(subcategoryPicker.type).toEqual(DropDown);
       expect(subcategoryPicker.props.options).toEqual([subcategories[0], subcategories[2], subcategories[4]]);
     });
   });
--- web/src/js/components/common/controls/subcategory-picker.js
+++ web/src/js/components/common/controls/subcategory-picker.js
@@ -1,28 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Picker from './picker';
-require('../../../../css/picker.scss');
+import React, { PropTypes } from 'react';
+import DropDown from './drop-down';
 
 export default class SubcategoryPicker extends React.Component {
 
   subcategoriesForCategory() {
-    return this.props.groupedCategories.map((categoryType) => {
-      return categoryType.categories.filter(category => category.id === this.props.categoryId);
-    }).filter(array => array.length === 1)[0][0].subcategories;
+    return this.props.groupedCategories.map(categoryType =>
+      categoryType.categories.filter(category => category.id === this.props.categoryId)
+    ).filter(array => array.length === 1)[0][0].subcategories;
   }
 
   render() {
     const subcategories = this.subcategoriesForCategory();
 
     return (
-      <Picker value={this.props.value} options={subcategories} onChange={this.props.onChange} />
+      <DropDown value={this.props.value} options={subcategories} allowUnassigned onChange={this.props.onChange} />
     );
   }
 }
 
 SubcategoryPicker.propTypes = {
-  value: React.PropTypes.number,
-  categoryId: React.PropTypes.number.isRequired,
-  groupedCategories: React.PropTypes.array.isRequired,
-  onChange: React.PropTypes.func.isRequired,
+  value: PropTypes.number,
+  categoryId: PropTypes.number.isRequired,
+  groupedCategories: PropTypes.arrayOf(PropTypes.shape({})).isRequired,
+  onChange: PropTypes.func.isRequired,
 };
