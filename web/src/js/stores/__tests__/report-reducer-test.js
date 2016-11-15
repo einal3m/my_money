c7590f04f316c3c0cd0111a75e8c148bbab97263
Melanie Matthews <melanie.a.matthews@gmail.com>
2016-11-15 13:50:39 +1100

    Add loaded to report reducer

--- web/src/js/stores/__tests__/report-reducer-test.js
+++ web/src/js/stores/__tests__/report-reducer-test.js
@@ -1,22 +1,46 @@
-import { List, Map, fromJS } from 'immutable';
+import { Map } from 'immutable';
 import reportReducer from '../report-reducer';
+import { GET_REPORT, SET_ACCOUNT_BALANCE_REPORT, SET_TRANSACTION_REPORT } from '../../actions/report-actions';
 
 describe('ReportReducer', () => {
   it('has a default state', () => {
     const state = reportReducer();
     expect(state.get('accountBalances').toJS()).toEqual({});
+    expect(state.get('transactions').toJS()).toEqual([]);
+    expect(state.get('totals').toJS()).toEqual([]);
+    expect(state.get('loaded')).toEqual(false);
+  });
+
+  describe('GET_REPORT', () => {
+    it('sets loading to true', () => {
+      const action = { type: GET_REPORT };
+      const state = reportReducer(Map({ loaded: true }), action);
+      expect(state.get('loaded')).toEqual(false);
+    });
   });
 
   describe('SET_ACCOUNT_BALANCE_REPORT', () => {
-    it('sets the balances for the account', () => {
-      let action = { type: 'SET_ACCOUNT_BALANCE_REPORT', accountId: 3, report: ['report3'] };
+    it('sets the balances for the account, and sets loading to false', () => {
+      let action = { type: SET_ACCOUNT_BALANCE_REPORT, accountId: 3, report: ['report3'] };
       const state = reportReducer(undefined, action);
       expect(state.get('accountBalances').get(3).toJS()).toEqual(['report3']);
+      expect(state.get('loaded')).toEqual(true);
 
-      action = { type: 'SET_ACCOUNT_BALANCE_REPORT', accountId: 4, report: ['report4'] };
+      action = { type: SET_ACCOUNT_BALANCE_REPORT, accountId: 4, report: ['report4'] };
       const nextState = reportReducer(state, action);
       expect(nextState.get('accountBalances').get(3).toJS()).toEqual(['report3']);
       expect(nextState.get('accountBalances').get(4).toJS()).toEqual(['report4']);
+      expect(state.get('loaded')).toEqual(true);
+    });
+  });
+
+  describe('SET_TRANSACTION_REPORT', () => {
+    it('sets the transactions and totals, and sets loading to false', () => {
+      const action = { type: SET_TRANSACTION_REPORT, transactions: ['transaction'], totals: ['total'] };
+      const state = reportReducer(undefined, action);
+      expect(state.get('transactions').toJS()).toEqual(['transaction']);
+      expect(state.get('totals').toJS()).toEqual(['total']);
+      expect(state.get('loaded')).toEqual(true);
     });
   });
 });
--- web/src/js/stores/report-reducer.js
+++ web/src/js/stores/report-reducer.js
@@ -1,17 +1,30 @@
 import { Map, List, fromJS } from 'immutable';
+import { GET_REPORT, SET_ACCOUNT_BALANCE_REPORT, SET_TRANSACTION_REPORT } from '../actions/report-actions';
 
 const INITIAL_STATE = Map({
   accountBalances: Map({}),
+  transactions: List(),
+  totals: List(),
+  loaded: false,
 });
 
 export default function reducer(state = INITIAL_STATE, action = { type: 'NO_ACTION' }) {
   switch (action.type) {
-    case 'SET_ACCOUNT_BALANCE_REPORT':
+    case GET_REPORT:
+      return state.set('loaded', false);
+    case SET_ACCOUNT_BALANCE_REPORT:
       return setAccountBalances(state, action.accountId, action.report);
+    case SET_TRANSACTION_REPORT:
+      return setTransactionReport(state, action.transactions, action.totals);
+    default:
+      return state;
   }
-  return state;
 }
 
 function setAccountBalances(state, accountId, report) {
-  return state.set('accountBalances', state.get('accountBalances').set(accountId, fromJS(report)));
+  return state.set('loaded', true).set('accountBalances', state.get('accountBalances').set(accountId, fromJS(report)));
+}
+
+function setTransactionReport(state, transactions, totals) {
+  return state.set('loaded', true).set('transactions', fromJS(transactions)).set('totals', fromJS(totals));
 }
