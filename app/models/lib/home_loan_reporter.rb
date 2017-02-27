cbccece19c8fcda543091f8d25625feaa9a31a2a
Melanie Matthews <melanie.a.matthews@gmail.com>
2017-02-27 20:05:52 +1100

    prepend loan reporter with balances from account starting date.

--- app/models/lib/home_loan_reporter.rb
+++ app/models/lib/home_loan_reporter.rb
@@ -1,3 +1,5 @@
+require 'lib/date_range'
+
 module Lib
   class HomeLoanReporter
     attr_reader :account
@@ -9,7 +11,7 @@ module Lib
     def execute
       {
         minimum_repayment: loan_calculator.minimum_repayment,
-        minimum_amortization: loan_calculator.minimum_amortization
+        minimum_amortization: eod_balance.drop(1).concat(loan_calculator.minimum_amortization)
       }
     end
 
@@ -17,6 +19,20 @@ module Lib
 
     def loan_calculator
       @loan_calculator ||= Lib::LoanCalculator.new(@account)
+    end
+
+    def eod_balance
+      @eod_balance ||= balance_search.eod_balance.map { |balance| [balance[0], -balance[1]] }
+    end
+
+    def balance_search
+      @balance_search ||= Lib::BalanceSearch.new(account: @account, date_range: start_of_loan_to_yesterday)
+    end
+
+    def start_of_loan_to_yesterday
+      @start_of_loan_to_yesterday ||= Lib::CustomDateRange.new(
+        from_date: @account.starting_date.strftime('%Y-%m-%d'), to_date: Date.yesterday.strftime('%Y-%m-%d')
+      )
     end
   end
 end
--- spec/lib/home_loan_reporter_spec.rb
+++ spec/lib/home_loan_reporter_spec.rb
@@ -1,14 +1,26 @@
 require 'rails_helper'
+require 'lib/date_range'
 
 RSpec.describe Lib::HomeLoanReporter, type: :class do
   before :each do
-    @account = FactoryGirl.create(:account)
+    @account = FactoryGirl.create(:account, starting_date: '2016-01-01')
     @reporter = Lib::HomeLoanReporter.new(@account)
 
     calculator = instance_double Lib::LoanCalculator
     expect(Lib::LoanCalculator).to receive(:new).and_return(calculator)
     expect(calculator).to receive(:minimum_repayment).and_return(8561)
-    expect(calculator).to receive(:minimum_amortization).and_return([1, 2, 3])
+    expect(calculator).to receive(:minimum_amortization).and_return([['2017-01-01', 3], ['2017-02-28', 4]])
+
+    allow(Date).to receive(:yesterday).and_return(Date.new(2016, 12, 31))
+
+    dr = instance_double Lib::CustomDateRange
+    expect(Lib::CustomDateRange).to receive(:new).with(from_date: '2016-01-01', to_date: '2016-12-31').and_return(dr)
+
+    balance_search = instance_double Lib::BalanceSearch
+    expect(Lib::BalanceSearch).to receive(:new).with(account: @account, date_range: dr).and_return(balance_search)
+    expect(balance_search).to receive(:eod_balance).and_return(
+      [['2016-01-01', 0], ['2016-01-02', -1], ['2016-02-28', -2]]
+    )
 
     @result = @reporter.execute
   end
@@ -27,7 +39,9 @@ RSpec.describe Lib::HomeLoanReporter, type: :class do
 
   describe 'minimum repayment ammortization' do
     it 'returns the amortization' do
-      expect(@result[:minimum_amortization]).to eq([1, 2, 3])
+      expect(@result[:minimum_amortization]).to eq(
+        [['2016-01-02', 1], ['2016-02-28', 2], ['2017-01-01', 3], ['2017-02-28', 4]]
+      )
     end
   end
 end
