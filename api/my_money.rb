afd2d20efe06b1108588d489aafb0f72bc5966f1
Melanie Matthews <melanie.a.matthews@gmail.com>
2017-04-04 22:43:12 +1000

    add rubocop and rake tasks

--- api/my_money.rb
+++ api/my_money.rb
@@ -1,9 +1,9 @@
-require "roda"
+require 'roda'
 
 class MyMoney < Roda
   plugin :multi_route
   plugin :json
-  Dir['./routes/*.rb'].each{|f| require f}
+  Dir['./routes/*.rb'].each { |f| require f }
 
   route(&:multi_route)
 end
--- api/spec/routes/account_types_spec.rb
+++ api/spec/routes/account_types_spec.rb
@@ -11,11 +11,11 @@ RSpec.describe '/account_types' do
   it 'calls the account type query and returns the result' do
     query = instance_double AccountTypeQuery
     expect(AccountTypeQuery).to receive(:new).and_return(query)
-    expect(query).to receive(:execute).and_return({ account_types: ['one'] })
+    expect(query).to receive(:execute).and_return(account_types: ['one'])
 
     get '/account_types'
 
     expect(last_response.status).to eq(200)
-    expect(JSON.parse(last_response.body)).to eq({ 'account_types' => ['one'] })
+    expect(JSON.parse(last_response.body)).to eq('account_types' => ['one'])
   end
 end
