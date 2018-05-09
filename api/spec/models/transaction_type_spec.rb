fe3830aee9e5543a59f60581decf3e02d47647e6
Melanie Matthews <melanie.a.matthews@gmail.com>
2018-05-09 20:15:39 +1000

    add transaction type to api

---
+++ api/spec/models/transaction_type_spec.rb
@@ -0,0 +1,38 @@
+require_relative '../../models/transaction_type'
+
+RSpec.describe TransactionType, type: :model do
+  it 'Share Purchase' do
+    transaction_type = TransactionType::SharePurchase.new
+    expect(transaction_type.id).to eq(1)
+    expect(transaction_type.code).to eq('purchase')
+    expect(transaction_type.name).to eq('Purchase')
+  end
+
+  it 'Share Dividend' do
+    transaction_type = TransactionType::Dividend.new
+    expect(transaction_type.id).to eq(2)
+    expect(transaction_type.code).to eq('dividend')
+    expect(transaction_type.name).to eq('Dividend')
+  end
+
+  it 'Unit Price Update' do
+    transaction_type = TransactionType::UnitPriceUpdate.new
+    expect(transaction_type.id).to eq(3)
+    expect(transaction_type.code).to eq('unit_price_update')
+    expect(transaction_type.name).to eq('Unit Price Update')
+  end
+
+  it 'Share Sale' do
+    transaction_type = TransactionType::ShareSale.new
+    expect(transaction_type.id).to eq(4)
+    expect(transaction_type.code).to eq('sale')
+    expect(transaction_type.name).to eq('Sale')
+  end
+
+  it 'Bank Transaction' do
+    transaction_type = TransactionType::BankTransaction.new
+    expect(transaction_type.id).to eq(5)
+    expect(transaction_type.code).to eq('bank_transaction')
+    expect(transaction_type.name).to eq('Bank Transaction')
+  end
+end
