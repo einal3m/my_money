c651351e967a9c07897eda270df32a2cd04ab703
Melanie Matthews <melanie.a.matthews@gmail.com>
2021-04-20 22:32:30 +1000

    Update react-select-plus to latest react-select

--- spec/javascript/components/common/controls/Select.test.js
+++ spec/javascript/components/common/controls/Select.test.js
@@ -1,81 +1,168 @@
 import React from 'react';
-import ReactSelect from 'react-select-plus';
-import shallowRenderer from '../../../../util/__tests__/shallow-renderer';
-import Select from '../select';
+import { render } from '@testing-library/react';
+import selectEvent from 'react-select-event';
+import Select from 'components/common/controls/Select';
 
 describe('Select', () => {
-  const options = [
-    { id: 1, name: 'Honey' },
-    { id: 2, name: 'Chocolate' },
-    { id: 3, name: 'Cake' },
-  ];
-  let onChangeSpy;
-  let select;
+  const onChangeSpy = jasmine.createSpy('onChangeSpy');
 
-  describe('render', () => {
-    beforeEach(() => {
-      onChangeSpy = jasmine.createSpy('onChangeSpy');
-    });
+  describe('simple options', () => {
+    const options = [
+      { id: 1, name: 'Honey' },
+      { id: 2, name: 'Chocolate' },
+      { id: 3, name: 'Cake' },
+    ];
 
-    it('renders a react select', () => {
-      select = shallowRenderer(<Select name="dropDown" value={2} options={options} onChange={onChangeSpy} />);
-
-      expect(select.type).toEqual(ReactSelect);
-      expect(select.props.name).toEqual('dropDown');
-      expect(select.props.value).toEqual(2);
-      expect(select.props.clearable).toEqual(false);
-      expect(select.props.placeholder).toEqual('Please select...');
-    });
-
-    it('renders a react select with converted options', () => {
-      select = shallowRenderer(<Select name="dropDown" value={2} options={options} onChange={onChangeSpy} />);
-
-      expect(select.props.options).toEqual([
-        { value: 1, label: 'Honey' },
-        { value: 2, label: 'Chocolate' },
-        { value: 3, label: 'Cake' },
-      ]);
-    });
-
-    it('renders a react select with converted grouped options', () => {
-      const groupedOptions = [
-        { name: 'Group One', options: [{ id: 1, name: 'Honey' }] },
-        { name: 'Group Two', options: [{ id: 2, name: 'Chocolate' }, { id: 3, name: 'Cake' }] },
-      ];
-
-      select = shallowRenderer(
-        <Select name="dropDown" value={2} groupedOptions={groupedOptions} onChange={onChangeSpy} />
+    it('displays a placeholder if value is not defined', () => {
+      const {getByText} = render(
+        <Select
+          name="dropDown"
+          options={options}
+          onChange={onChangeSpy}
+          allowUnassigned
+        />
       );
 
-      expect(select.props.options).toEqual([
-        { label: 'Group One', options: [{ value: 1, label: 'Honey' }] },
-        { label: 'Group Two', options: [{ value: 2, label: 'Chocolate' }, { value: 3, label: 'Cake' }] },
-      ]);
+      expect(getByText('Un-assigned')).toBeInTheDocument();
     });
 
-    it('renders with un-assigned placeholder and not clearable if no value and allowUnassigned is true', () => {
-      select = shallowRenderer(<Select name="dropDown" options={options} allowUnassigned onChange={onChangeSpy} />);
+    it('displays a different placeholder if value is mandatory', () => {
+      const {getByText} = render(
+        <Select
+          name="dropDown"
+          options={options}
+          onChange={onChangeSpy}
+        />
+      );
 
-      expect(select.props.placeholder).toEqual('Un-assigned');
-      expect(select.props.clearable).toEqual(true);
+      expect(getByText('Please select...')).toBeInTheDocument();
     });
-  });
 
-  describe('on select', () => {
-    it('calls the onChange prop with selected option', () => {
-      select = shallowRenderer(<Select name="dropDown" value={2} options={options} onChange={onChangeSpy} />);
+    it('displays the selected value if one is provided', () => {
+      const {getByText, queryByText} = render(
+        <Select
+          name="dropDown"
+          value={2}
+          options={options}
+          onChange={onChangeSpy}
+        />
+      );
 
-      select.props.onChange({ value: 1, label: 'Honey' });
+      expect(getByText('Chocolate')).toBeInTheDocument();
+      expect(queryByText('Honey')).not.toBeInTheDocument();
+      expect(queryByText('Cake')).not.toBeInTheDocument();
+    });
+
+    it('displays the placeholder if value is not in the options', () => {
+      const {getByText, queryByText} = render(
+        <Select
+          name="dropDown"
+          value={12}
+          options={options}
+          onChange={onChangeSpy}
+        />
+      );
+
+      expect(getByText('Please select...')).toBeInTheDocument();
+      expect(queryByText('Chocolate')).not.toBeInTheDocument();
+      expect(queryByText('Honey')).not.toBeInTheDocument();
+      expect(queryByText('Cake')).not.toBeInTheDocument();
+    });
+
+    it('displays the list of options when clicked', async () => {
+      const {getByText, container } = render(
+        <Select
+          name="dropDown"
+          value={2}
+          options={options}
+          onChange={onChangeSpy}
+        />
+      );
+
+      const dropdown = getByText('Chocolate')
+      await selectEvent.openMenu(dropdown);
+
+      const selectOptions = container.querySelectorAll(".react-select__option");
+
+      expect(selectOptions.length).toEqual(3);
+      expect(selectOptions[0].textContent).toEqual('Honey');
+      expect(selectOptions[1].textContent).toEqual('Chocolate');
+      expect(selectOptions[2].textContent).toEqual('Cake');
+    });
+
+    it('calls the onChange prop when an option is selected', async () => {
+      const { getByText } = render(
+        <Select
+          name="dropDown"
+          value={2}
+          options={options}
+          onChange={onChangeSpy}
+        />
+      );
+
+      const dropdown = getByText('Chocolate')
+      await selectEvent.select(dropdown, ['Honey']);
 
       expect(onChangeSpy).toHaveBeenCalledWith(1);
     });
+  });
 
-    it('calls the onChange prop with null when select cleared', () => {
-      select = shallowRenderer(<Select name="dropDown" value={2} options={options} onChange={onChangeSpy} />);
+  describe('grouped options', () => {
+    const groupedOptions = [
+      { name: 'Group One', options: [{ id: 1, name: 'Honey' }] },
+      { name: 'Group Two', options: [{ id: 2, name: 'Chocolate' }, { id: 3, name: 'Cake' }] },
+    ];
 
-      select.props.onChange(null);
+    it('displays the selected value if one is provided', () => {
+      const {getByText, queryByText} = render(
+        <Select
+          name="dropDown"
+          value={2}
+          groupedOptions={groupedOptions}
+          onChange={onChangeSpy}
+        />
+      );
 
-      expect(onChangeSpy).toHaveBeenCalledWith(null);
+      expect(getByText('Chocolate')).toBeInTheDocument();
+      expect(queryByText('Honey')).not.toBeInTheDocument();
+      expect(queryByText('Cake')).not.toBeInTheDocument();
+    });
+
+    it('displays the placeholder if value is not in the options', () => {
+      const {getByText, queryByText} = render(
+        <Select
+          name="dropDown"
+          value={12}
+          groupedOptions={groupedOptions}
+          onChange={onChangeSpy}
+        />
+      );
+
+      expect(getByText('Please select...')).toBeInTheDocument();
+      expect(queryByText('Chocolate')).not.toBeInTheDocument();
+      expect(queryByText('Honey')).not.toBeInTheDocument();
+      expect(queryByText('Cake')).not.toBeInTheDocument();
+    });
+
+    it('renders grouped options when clicked', async () => {
+
+      const { getByText, container } = render(
+        <Select
+          name="dropDown"
+          value={2}
+          groupedOptions={groupedOptions}
+          onChange={onChangeSpy}
+        />
+      );
+
+      const dropdown = getByText('Chocolate');
+      await selectEvent.openMenu(dropdown);
+
+      const selectGroups = container.querySelectorAll(".react-select__group");
+
+      expect(selectGroups.length).toEqual(2);
+      expect(selectGroups[0].textContent).toEqual('Group OneHoney');
+      expect(selectGroups[1].textContent).toEqual('Group TwoChocolateCake');
     });
   });
 });
