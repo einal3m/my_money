a6972ac7cbd11ef6686b815b04c29e89f963d170
Melanie Matthews <melanie.a.matthews@gmail.com>
2024-04-08 17:19:45 +1000

    chore: update to new rails/vite configs

---
+++ vite.config.ts
@@ -0,0 +1,23 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import RubyPlugin from "vite-plugin-ruby";
+
+console.log("i am here");
+console.log(resolve(__dirname, "app/javascript/actions"));
+
+export default defineConfig({
+  plugins: [RubyPlugin()],
+  resolve: {
+    alias: {
+      actions: resolve(__dirname, "app/javascript/actions"),
+      components: resolve(__dirname, "app/javascript/components"),
+      hooks: resolve(__dirname, "app/javascript/hooks"),
+      selectors: resolve(__dirname, "app/javascript/selectors"),
+      stores: resolve(__dirname, "app/javascript/stores"),
+      stylesheets: resolve(__dirname, "app/javascript/stylesheets"),
+      transformers: resolve(__dirname, "app/javascript/transformers"),
+      util: resolve(__dirname, "app/javascript/util"),
+      // "#root": resolve(__dirname),
+    },
+  },
+});
