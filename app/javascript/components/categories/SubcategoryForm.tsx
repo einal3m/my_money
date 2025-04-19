3b5c7f8fd590354f8db00c2f0427fdf86d0df9e8
Melanie Matthews <melanie.a.matthews@gmail.com>
2025-04-19 22:16:45 +1000

    refactor: use react-hook-form for subcategory form

---
+++ app/javascript/components/categories/SubcategoryForm.tsx
@@ -0,0 +1,57 @@
+import React from 'react'
+
+import { FormControl } from 'components/common/controls/NewFormControl'
+import { useForm, SubmitHandler, Controller } from 'react-hook-form'
+import { Subcategory, SubcategoryFormInput } from 'types/models'
+import { GroupedCategories } from 'hooks/useGroupedCategories'
+import GroupedCategorySelect from 'components/common/controls/GroupedCategorySelect'
+
+type SubcategoryFormProps = {
+  subcategory: Subcategory
+  groupedCategories: GroupedCategories[]
+  onSubmit: (data: SubcategoryFormInput) => void
+}
+
+export const SubcategoryForm = (props: SubcategoryFormProps) => {
+  const {
+    control,
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<SubcategoryFormInput>({ defaultValues: props.subcategory })
+
+  const onSubmit: SubmitHandler<SubcategoryFormInput> = (data) => {
+    props.onSubmit(data)
+  }
+
+  return (
+    <form id="modal-form" onSubmit={handleSubmit(onSubmit)}>
+      <FormControl
+        name="categoryId"
+        label="Category Type"
+        errorMessage={errors.categoryId?.message}
+      >
+        <Controller
+          name="categoryId"
+          control={control}
+          rules={{ required: 'Category is required' }}
+          render={({ field }) => (
+            <GroupedCategorySelect
+              name="categoryId"
+              value={field.value}
+              groupedCategories={props.groupedCategories}
+              onChange={field.onChange}
+            />
+          )}
+        />
+      </FormControl>
+      <FormControl name="name" label="name" errorMessage={errors.name?.message}>
+        <input
+          id="name"
+          className="form-control"
+          {...register('name', { required: 'Name is required.' })}
+        />
+      </FormControl>
+    </form>
+  )
+}
